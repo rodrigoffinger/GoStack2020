192f0f793f34a54c3fc5c2c5e38b47f8ed8803fc
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2020-06-30 19:15:13 -0300

    GoBarber Backend - DB e cadastro de usuários

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,30 +1,29 @@
 import { Router } from "express";
+import { getCustomRepository } from "typeorm";
 import { parseISO } from "date-fns";
 
 import AppointmentsRepository from "../repositories/AppointmentRepository";
 import CreateAppointmentService from "../services/CreateAppointmentService";
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get("/", (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get("/", async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post("/", (request, response) => {
+appointmentsRouter.post("/", async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(
-      appointmentsRepository
-    );
+    const createAppointment = new CreateAppointmentService();
 
-    const appointment = createAppointment.execute({
-      provider,
+    const appointment = await createAppointment.execute({
+      provider_id,
       date: parsedDate,
     });
 
