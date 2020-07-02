fdfb7d7edf5cda845edfc676b0186f0e05cfabbd
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2020-07-02 18:18:02 -0300

    React - Rotas & Styled Components

--- primeiro-projeto-node/src/app.ts
+++ primeiro-projeto-node/src/app.ts
@@ -25,7 +25,7 @@ app.use(
 
     console.error(err);
 
-    return response.status(400).json({
+    return response.status(500).json({
       status: "error",
       message: "Internal server error",
     });
