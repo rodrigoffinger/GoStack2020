39ce18cb8d44c15eb887a76803e240e4c122cc8d
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2020-08-26 11:24:37 -0300

    Agendamento

--- gobarber-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ gobarber-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -15,9 +15,14 @@ describe('CreateAppointment', () => {
   });
 
   it('should be able to create a new appoitment', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 7, 26, 12).getTime();
+    });
+
     const appointment = await createAppointment.execute({
-      date: new Date(),
+      date: new Date(2020, 7, 26, 13),
       provider_id: '1234',
+      user_id: '2234',
     });
 
     expect(appointment).toHaveProperty('id');
@@ -25,17 +30,73 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appoitments on the same time', async () => {
-    const appointmentDate = new Date(2020, 7, 19, 11);
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 7, 26, 12).getTime();
+    });
+
+    const appointmentDate = new Date(2020, 7, 26, 13);
 
     const appointment = await createAppointment.execute({
       date: appointmentDate,
       provider_id: '1234',
+      user_id: '2234',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '1234',
+        user_id: '2234',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment on a past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 7, 26, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 7, 26, 10),
+        provider_id: '1234',
+        user_id: '2234',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 7, 26, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 7, 26, 13),
+        provider_id: '1234',
+        user_id: '1234',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment before 8am and after 5pm', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 7, 26, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 7, 27, 6),
+        provider_id: '1234',
+        user_id: '2234',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 7, 27, 18),
+        provider_id: '1234',
+        user_id: '2234',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
---
+++ gobarber-backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,105 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderMonthAvailabilityService: ListProviderMonthAvailabilityService;
+
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderMonthAvailabilityService = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the month availability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 6, 10, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 9, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 10, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 15, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 17, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 7, 27, 8, 0, 0),
+    });
+
+    const availability = await listProviderMonthAvailabilityService.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 8,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 25, available: true },
+        { day: 26, available: false },
+        { day: 27, available: true },
+        { day: 28, available: true },
+      ]),
+    );
+  });
+});
