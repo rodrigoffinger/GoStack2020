4bb910e479044cc1202335965d9326c00c4954c0
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2020-08-26 16:02:22 -0300

    Agenda do prestador

--- gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,16 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import ensureAutenticated from "@modules/users/infra/http/middleware/ensureAuthenticated";
-import AppointmentsController from "../controllers/AppointmentsController";
+import ensureAutenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAutenticated);
 
-appointmentsRouter.post("/", appointmentsController.create);
+appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
---
+++ gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 7, 26, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointmentsService.execute({
+      provider_id: 'provider',
+      day: 26,
+      year: 2020,
+      month: 8,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
