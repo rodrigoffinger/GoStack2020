3b043729b596ea47ba1214f4249bac4b46ed1fa4
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2020-08-28 15:12:57 -0300

    Amazon SES

---
+++ gobarber-backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'rodrigo@example.com',
+      name: 'Rodrigo',
+    },
+  },
+} as IMailConfig;
