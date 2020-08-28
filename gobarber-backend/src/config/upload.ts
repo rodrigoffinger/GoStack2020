9cf6393d480fb71622726af9183ef37fe2e83ce9
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2020-08-28 16:07:25 -0300

    Amazon S3

--- gobarber-backend/src/config/upload.ts
+++ gobarber-backend/src/config/upload.ts
@@ -1,19 +1,49 @@
-import path from "path";
-import crypto from "crypto";
-import multer from "multer";
+import path from 'path';
+import crypto from 'crypto';
+import multer, { StorageEngine } from 'multer';
 
-const tmpFolder = path.resolve(__dirname, "..", "..", "temp");
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 export default {
-  tmpFolder: tmpFolder,
-  uploadsFolder: path.resolve(tmpFolder, "uploads"),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString("hex");
-      const fileName = `${fileHash}-${file.originalname}`;
+  driver: process.env.STORAGE_DRIVER,
 
-      return callback(null, fileName);
+  tmpFolder: tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'aws-gobarber',
     },
-  }),
-};
+  },
+} as IUploadConfig;
