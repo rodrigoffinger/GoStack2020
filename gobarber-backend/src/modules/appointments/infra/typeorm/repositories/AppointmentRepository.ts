9ca974b05363b638213b5e9749fdcf56c2d1dbaa
Rodrigo Finger <rodrigo.finger@digitaldesk.com.br>
2020-09-03 17:47:07 -0300

    Dashboard funcional

--- gobarber-backend/src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
+++ gobarber-backend/src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -63,6 +63,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         ),
       },
       relations: ['user'],
+      order: { date: 'ASC' },
     });
 
     return appointments;
--- gobarber-web/src/hooks/auth.tsx
+++ gobarber-web/src/hooks/auth.tsx
@@ -6,13 +6,19 @@ interface SignInCredentials {
   password: string;
 }
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -25,6 +31,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -38,6 +46,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
